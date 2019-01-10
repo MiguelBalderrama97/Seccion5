83c7d15f87535a78f4436f2ae1dab65ee6822c14
MiguelBalderrama97 <37118731+MiguelBalderrama97@users.noreply.github.com>
2019-01-09 23:20:08 -0700

    Validación del login
    Crear todos los métodos en la clase LoginActivity.java  para validar el email y el pass.

--- app/src/main/java/com/example/miguel/seccion5/LoginActivity.java
+++ app/src/main/java/com/example/miguel/seccion5/LoginActivity.java
@@ -1,13 +1,72 @@
 package com.example.miguel.seccion5;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Patterns;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Switch;
+import android.widget.Toast;
 
 public class LoginActivity extends AppCompatActivity {
+
+    private EditText editTextEmail;
+    private EditText editTextPassword;
+    private Switch switchRemember;
+    private Button btnLogin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        bindUI();
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String email = editTextEmail.getText().toString();
+                String password = editTextPassword.getText().toString();
+                if(login(email,password)){
+                    goToMain();
+                }
+            }
+        });
+    }
+
+    private void bindUI(){
+        editTextEmail = findViewById(R.id.editTextEmail);
+        editTextPassword = findViewById(R.id.editTextPassword);
+        switchRemember = findViewById(R.id.switchRemember);
+        btnLogin = findViewById(R.id.buttonLogin);
+    }
+
+    private boolean login(String email, String password){
+        if(!isValidEmail(email)){
+            Toast.makeText(LoginActivity.this, "Emails is not valid, please try again", Toast.LENGTH_LONG).show();
+            return false;
+        }else if(!isValidPassword(password)){
+            Toast.makeText(LoginActivity.this, "Password is not valid, please try again", Toast.LENGTH_LONG).show();
+            return false;
+        }else{
+            return true;
+        }
+    }
+
+    private boolean isValidEmail(String email){
+        return !TextUtils.isEmpty(email) && Patterns.EMAIL_ADDRESS.matcher(email).matches();
+    }
+
+    private boolean isValidPassword(String password){
+        return password.length() > 4;
+    }
+
+    private void goToMain(){
+        Intent intent = new Intent(this, MainActivity.class);
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        startActivity(intent);
     }
 }
--- app/src/main/java/com/example/miguel/seccion5/MainActivity.java
+++ app/src/main/java/com/example/miguel/seccion5/MainActivity.java
@@ -1,40 +1,22 @@
 package com.example.miguel.seccion5;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.TextUtils;
 import android.util.Patterns;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Switch;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
-    private EditText editTextEmail;
-    private EditText editTextPassword;
-    private Switch switchRemember;
-    private Button btnLogin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        bindUI();
     }
 
-    private void bindUI(){
-        editTextEmail = findViewById(R.id.editTextEmail);
-        editTextPassword = findViewById(R.id.editTextPassword);
-        switchRemember = findViewById(R.id.switchRemember);
-        btnLogin = findViewById(R.id.buttonLogin);
-    }
-
-    private boolean isValidEmail(String email){
-        return !TextUtils.isEmpty(email) && Patterns.EMAIL_ADDRESS.matcher(email).matches();
-    }
-
-    private boolean isValidPassword(String password){
-        return password.length() > 4;
-    }
 }
